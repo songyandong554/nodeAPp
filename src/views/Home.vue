662d7a6f4677085b8eb0dc5ac41e7c19f9aa03d3
syd <158170907@qq.com>
2025-07-02 17:32:35 +0800

    fix: 修正首页欢迎语中的错别字

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>欢迎来到首页{{ test.message }}</h1>
+    <h1>欢迎来万五到首页{{ test.message }}</h1>
     <p>这是一个使用2222222222222222222ue 3 + TypeScript + Vite 构建的应用</p>
 
     <div class="navigation">
